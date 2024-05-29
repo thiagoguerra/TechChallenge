a5c86c6d3ba8ef1b728a94f38b6e755b70e23989
Thiago Furuta <thiago.furuta@stagwellgtg.com>
2024-05-28 21:59:20 -0300

    Add project files.

---
+++ TechChallenge/Program.cs
@@ -0,0 +1,59 @@
+using Microsoft.EntityFrameworkCore;
+using Infrastructure.Repositories;
+using Infrastructure.Repositories.Interface;
+using Application.Applications.Interfaces;
+using Application.Applications;
+using FluentValidation.AspNetCore;
+using System.Reflection;
+using Core.Entities;
+using Core.Validations;
+using FluentValidation;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var configuration = new ConfigurationBuilder()
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+// Add services to the container.
+
+builder.Services.AddFluentValidationAutoValidation().AddFluentValidationClientsideAdapters();
+builder.Services.AddControllers()
+    .ConfigureApiBehaviorOptions(options =>
+    {
+        options.SuppressModelStateInvalidFilter = true;
+    });
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(c =>
+{
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = $"{Path.Combine(AppContext.BaseDirectory, xmlFile)}";
+    c.IncludeXmlComments(xmlPath);
+});
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(configuration.GetConnectionString("ConnectionString"));
+}, ServiceLifetime.Scoped);
+
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+builder.Services.AddScoped<IContactApplication, ContactApplication>();
+builder.Services.AddScoped<IValidator<Contact>, ContactValidator>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
